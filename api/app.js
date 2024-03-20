83dc581c9320470063468a51fea689db1798b6f6
taoufikbnr <69259869+taoufikbnr@users.noreply.github.com>
2024-03-20 15:38:45 +0100

    refractor : Added Express Router for Tasks

--- api/app.js
+++ api/app.js
@@ -1,31 +1,15 @@
 require("dotenv").config({ path: "./config/.env" });
 const express = require("express");
 const connectDB = require("./config/connectDB");
-const Task = require("./models/Task");
-const Router = express.Router();
+const taskRoute = require("./routes/task");
 
 const app = express();
 app.use(express.json());
 
-app.post("/api/tasks", async (req,res)=>{
- try {
-    const task = await Task.create(req.body);
-    res.status(200).json({msg:"task was added",task});
- } catch (error) {
-  res.status(400).json({msg:error.message});
- }
-})
-app.get("/api/getTasks", async (req,res)=>{
-  try {
-     const tasks = await Task.find();
-     res.status(200).json({msg:"Tasks fetched",tasks});
 
-  } catch (error) {
-   res.status(400).json({msg:error.message});
-  }
- })
-   
-  const startServer = async()=>{
+app.use("/api",taskRoute)
+
+const startServer = async()=>{
     try {
       await connectDB();
       app.listen(process.env.PORT, (err) => {
