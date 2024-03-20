7c2e3230066ae6768c8524a2ed6a124cd0745394
taoufikbnr <69259869+taoufikbnr@users.noreply.github.com>
2024-03-20 16:31:01 +0100

    feat : Added DELETE task - PUT task End points

--- api/controllers/taskController.js
+++ api/controllers/taskController.js
@@ -9,11 +9,48 @@ exports.createTask = async (req,res)=>{
      res.status(500).json({msg:error.message});
     }
    }
-   
+
 exports.getTasks = async(req,res)=>{
     try {
        const tasks = await Task.find();
        res.status(200).json({msg:"Tasks fetched",tasks});
+  
+    } catch (error) {
+     res.status(500).json({msg:error.message});
+    }         
+}
+
+   
+exports.getTask = async(req,res)=>{
+    try {
+       const task = await Task.findById(req.params.id);
+       if(!task) return res.status(404).json("Task doesn't exist");
+       res.status(200).json({msg:"Task fetched",task});
+  
+    } catch (error) {
+     res.status(500).json({msg:error.message});
+    }         
+}
+
+exports.deleteTask = async(req,res)=>{
+    try {
+       const task = await Task.findByIdAndDelete(req.params.id);
+       if(!task) return res.status(404).json("Task doesn't exist");
+
+       res.status(200).json({msg:"Task deleted successfully"});
+  
+    } catch (error) {
+     res.status(500).json({msg:error.message});
+    }         
+}
+
+
+exports.updateTask = async(req,res)=>{
+    try {
+       const task = await Task.findByIdAndUpdate(req.params.id,{$set:req.body},{new:true,runValidators:true});
+       if(!task) return res.status(404).json("Task doesn't exist");
+
+       res.status(200).json({msg:"Task updated successfully",task});
   
     } catch (error) {
      res.status(500).json({msg:error.message});
--- api/routes/task.js
+++ api/routes/task.js
@@ -1,12 +1,18 @@
 const express = require("express");
-const { getTasks, createTask } = require("../controllers/taskController");
+const { getTasks, createTask, getTask, deleteTask, updateTask } = require("../controllers/taskController");
 
 const Router = express.Router();
 
-
-Router.post("/tasks",createTask)
+Router.post("/tasks",createTask);
 
 Router.get("/tasks",getTasks);
+
+Router.get("/task/:id",getTask);
+
+Router.delete("/task/:id",deleteTask);
+
+Router.put("/task/:id",updateTask);
+
 
 
 module.exports = Router;
