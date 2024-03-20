07dab212694f37fbbac1e1bd8213585df6801900
taoufikbnr <69259869+taoufikbnr@users.noreply.github.com>
2024-03-20 14:41:13 +0100

    feat : Added create Task Api - Added Fetch tasks Api

--- api/app.js
+++ api/app.js
@@ -1,17 +1,30 @@
 require("dotenv").config({ path: "./config/.env" });
 const express = require("express");
 const connectDB = require("./config/connectDB");
-const { default: mongoose } = require("mongoose");
+const Task = require("./models/Task");
 const Router = express.Router();
 
 const app = express();
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-  res.send("home");
+app.post("/api/tasks", async (req,res)=>{
+ try {
+    const task = await Task.create(req.body);
+    res.status(200).json({msg:"task was added",task});
+ } catch (error) {
+  res.status(400).json({msg:error.message});
+ }
 })
+app.get("/api/getTasks", async (req,res)=>{
+  try {
+     const tasks = await Task.find();
+     res.status(200).json({msg:"Tasks fetched",tasks});
 
-
+  } catch (error) {
+   res.status(400).json({msg:error.message});
+  }
+ })
+   
   const startServer = async()=>{
     try {
       await connectDB();
